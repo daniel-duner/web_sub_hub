3682ae7b00a52ccac8e75e4a2e7ddf0d2407dce8
daniel-duner <daniel_duner@hotmail.com>
2020-02-24 18:24:39 +0100

    Added som limitations to this project

--- hub/main.go
+++ hub/main.go
@@ -1,6 +1,5 @@
 package main
 
-
 var subscriptions = make(map[string]subscription)
 
 func main() {
@@ -8,10 +7,9 @@ func main() {
 	connect()
 }
 
-/*	LIMITS
+/*	SOME LIMITATIONS
 	- It is not possible to be subscribed to more than one topic in this solution
-	- Subscriptions are stored in memory and will (just a map)
-	- The solution does not handle lease_seconds
-
-
+	- Subscriptions are stored in memory and will be lost on termination (just a map)
+	- The solution does not handle lease_seconds and subscription "time to live"
+	- No unit testing, only testing complete transactions through postman & curls
 */
